677d06bbd66823b761b6caddb6ca33f800f3f434
Jordan J. Carrillo Solis <jordan.jcarrillo.s@gmail.com>
2020-11-02 22:53:12 -0600

    BrowserDriverFactory class added

--- src/main/java/com/herokuapp/theinternet/base/BaseTest.java
+++ src/main/java/com/herokuapp/theinternet/base/BaseTest.java
@@ -1,8 +1,6 @@
 package com.herokuapp.theinternet.base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -17,30 +15,9 @@ public class BaseTest {
 	@Parameters("browser")
 	@BeforeMethod(alwaysRun = true)
 	public void setup(@Optional("chrome") String browser) {
-
-		switch (browser.toLowerCase()) {
-
-		case "chrome":
-			// Create Chrome Driver
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-			driver = new ChromeDriver();
-			System.out.println("Google Chrome Webdriver Created.");
-			break;
-
-		case "firefox":
-			// Create Firefox Driver
-			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
-			driver = new FirefoxDriver();
-			System.out.println("Firefox Webdriver Created.");
-			break;
-
-		default:
-			// Create Chrome Driver
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-			driver = new ChromeDriver();
-			System.out.println("Google Chrome Webdriver Created.");
-			break;
-		}
+		
+		BrowserDriverFactory facftory = new BrowserDriverFactory(browser);
+		driver = facftory.createDriver();
 
 		// Maximize window
 		driver.manage().window().maximize();
