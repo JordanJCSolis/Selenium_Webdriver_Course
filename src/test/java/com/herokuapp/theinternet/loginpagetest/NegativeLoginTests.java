fba27975d596de356a01634639f99b4274521f5e
Jordan J. Carrillo Solis <jordan.jcarrillo.s@gmail.com>
2020-11-23 01:36:12 -0600

    Data provider using CSV file added

--- src/test/java/com/herokuapp/theinternet/loginpagetest/NegativeLoginTests.java
+++ src/test/java/com/herokuapp/theinternet/loginpagetest/NegativeLoginTests.java
@@ -1,20 +1,32 @@
 package com.herokuapp.theinternet.loginpagetest;
 
+import java.util.Map;
+
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.herokuapp.theinternet.base.CsvDataProviders;
 import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.pages.LoginPageObject;
 import com.herokuapp.theinternet.pages.WelcomePageObject;
 
 public class NegativeLoginTests extends TestUtilities {
 
-	@Parameters({ "username", "password", "expectedMessage" })
-	@Test(priority = 1, enabled = true, groups = { "negativeTest", "smokeTest" })
+	@Test(dataProvider = "csvReader", dataProviderClass = CsvDataProviders.class,
+			priority = 1, enabled = true, groups = { "negativeTest", "smokeTest" })
 
-	public void negativeLoginTest(String username, String password, String expectedMessage) {
-		log.info("Starting Negative Login Test");
+	public void negativeLoginTest(Map<String, String> testData) {
+		
+		// Data
+		String testNumber = testData.get("testNumber");
+		String username = testData.get("username");
+		String password = testData.get("password");
+		String expectedMessage = testData.get("expectedMessage");
+		String testDescription = testData.get("testDescription");
+		
+		
+		log.info("Starting Negative Login Test # " + testNumber + 
+				" for " + testDescription);
 
 		// Open main page
 		WelcomePageObject welcomePage = new WelcomePageObject(driver, log);
